c84ab1d8aebc18252b04df6bb54234ad5ebf2e6d
angelajanebouw <angelabouw@yahoo.es>
2022-05-29 23:38:18 +0200

    Created RepositoryLayer and general repository

---
+++ SmartOnApp.WebAPI/RepositoryLayer/Repositories/Repository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartOnApp.WebAPI.RepositoryLayer.Interfaces;
+
+namespace SmartOnApp.WebAPI.RepositoryLayer.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly SmartOnDbContext _smartOnDbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(SmartOnDbContext smartOnDbContext)
+        {
+            _smartOnDbContext = smartOnDbContext;
+            _dbSet = _smartOnDbContext.Set<T>();
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _smartOnDbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
