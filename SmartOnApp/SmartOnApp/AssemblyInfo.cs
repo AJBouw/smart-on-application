30c8eb75773e054d27800b1d5f89f90994457c83
angelajanebouw <angelabouw@yahoo.es>
2022-05-29 21:50:10 +0200

    Initial commit XamarinForms

---
+++ SmartOnApp/SmartOnApp/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FA5Regular.otf", Alias = "FontAwesome")]
+[assembly: ExportFont("SamanthaRegular400.ttf", Alias = "Samantha")]
+[assembly: ExportFont("TimesNewRoman.otf")]
+[assembly: ExportFont("MaterialDesignIcons.ttf")] 
---
+++ SmartOnApp/SmartOnApp/Views/LandingPage/LandingPageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SmartOnApp.Views.LandingPage
+{	
+	public partial class LandingPageView : ContentView
+	{	
+		public LandingPageView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
