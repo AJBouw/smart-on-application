fcd7b9855aaacda5a46e0788151aa8f02bd34e86
angelajanebouw <angelabouw@yahoo.es>
2022-06-04 21:06:23 +0200

    Created GET request McuController Mcu include IoTDevices
    Created GenericRepository and UnitOfWorkRepository
    
    Created method McuRepository
    
    Refactored id's MariaDb

--- SmartOnApp.WebAPI/UserInterfaceLayer/Controllers/McuController.cs
+++ SmartOnApp.WebAPI/UserInterfaceLayer/Controllers/McuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SmartOnApp.Shared.DomainLayer.Models;
 using SmartOnApp.WebAPI.RepositoryLayer.Interfaces;
 
@@ -12,7 +13,25 @@ namespace SmartOnApp.WebAPI.UserInterfaceLayer.Controllers
     [ApiController]
     public class McuController : ControllerBase
     {
-        
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMcuRepository _mcuRepository;
 
+        public McuController(IUnitOfWork unitOfWork, IMcuRepository mcuRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _mcuRepository = mcuRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Mcu>> GetAllMcu()
+        {
+            return await _unitOfWork.mcu.GetAllAsync();
+        }
+
+        [HttpGet("include")]
+        public async Task<IEnumerable<Mcu>> GetAllMcuInclude()
+        {
+            return await _mcuRepository.GetAllMcuIncludeAsync();
+        }
     }
 }
